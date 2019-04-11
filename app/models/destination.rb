3754db0f3b20b51e41da15f6d6d0cb780bb7cf99
Madison Stankevich <madison.stankevich@flatironschool.com>
2019-04-11 16:37:43 -0700

    switched back to previous inheritance

--- app/models/destination.rb
+++ app/models/destination.rb
@@ -1,4 +1,4 @@
-class DestinationSerializer < ActiveModel::Serializer
+class Destination < ApplicationRecord
   has_many :favorites
   has_many :forecasts
   has_many :favorite_tags, through: :favorites
