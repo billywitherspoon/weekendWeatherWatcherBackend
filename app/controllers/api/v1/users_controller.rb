3e0dfd6a04c64d2e3cab60037561aa0c1cb5d211
github username <github email address>
2019-04-15 18:24:19 -0700

    added new route for logging in user

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -1,17 +1,26 @@
 class Api::V1::UsersController < ApplicationController
-   before_action :set_destination, only: [:show]
+   before_action :set_user, only: [:show]
 
    def index 
       @users = User.all 
       render json: @users
    end 
    
-   def show
+   def username
+      @user = User.find_by(username: params[:username])
+      if @user
+         render json: @user
+      else 
+         render json: {error: 'Sorry, username does not exist in our system'}
+      end 
+   end 
+
+   def show 
       render json: @user 
    end 
 
    def create 
-      @user = User.new(destination_params)
+      @user = User.new(user_params)
       if @user.valid?
          @user.save
          render json: @user 
@@ -22,11 +31,11 @@ class Api::V1::UsersController < ApplicationController
 
    private 
 
-   def destination_params
+   def user_params
       params.permit(:username, :password, :first_name, :last_name)
    end
 
-   def set_destination
+   def set_user
       @user = User.find(params[:id])
    end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Rails.application.routes.draw do
   namespace :api do
       namespace :v1 do
           resources :users, :tags, :destinations, :forecasts
+          get '/users/username/:username', to: 'users#username'
       end
   end
 end
